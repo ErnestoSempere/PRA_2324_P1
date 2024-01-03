1539c909104271ef2fb938ee4b1fb6149b3a8e9b
ErnestoSempere <146226839+ErnestoSempere@users.noreply.github.com>
2024-01-03 17:33:47 +0100

    Update ListArray.h

--- ListArray.h
+++ ListArray.h
@@ -47,5 +47,62 @@ class ListArray : public List<T> {
 		out << "]" << std::endl;
 		return out;
 	}
-    
+
+	void insert(int pos, T e) override{
+		if(pos > n || pos < 0){
+                        throw std::out_of_range("Posición fuera del rango del array.");
+                }
+    		if(n == max){
+			resize(n + 10);
+		}
+		for(int i = n - 1; i >= pos; i--){
+			arr[i + 1] = arr[i];
+		}
+		arr[pos] = e;
+		n++;
+	}
+
+	void append(T e) override{
+		insert(n, e);
+	}
+	
+	void prepend(T e) override{
+		insert(0, e);
+	}
+
+	T remove(int pos) override{
+		if(pos >= n || pos < 0){
+                	throw std::out_of_range("Posición fuera del rango del array.");
+                }
+		T aux = arr[pos];
+		for(int i = pos; i < n; i++){
+                        arr[i] = arr[i + 1];
+                }
+		n--;
+		return aux;
+	}
+
+	T get(int pos) override{
+		if(pos >= n || pos < 0){
+                        throw std::out_of_range("Posición fuera del rango del array.");
+                }
+                return arr[pos];
+	}
+
+	int search(T e) override{
+		for(int i = 0; i < n; i++){
+			if(e == arr[i]){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	bool empty() override{
+		return n == 0;
+	}
+
+	int size() override{
+		return n;
+	}
 };
